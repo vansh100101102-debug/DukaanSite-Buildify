c8b01c7255e5df1adcfe648161a387c83d2ac423
Vansh Chandna <vansh100101102@gmail.com>
2026-05-09 21:15:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: ''https://dukaansite-buildify.onrender.com'',
         changeOrigin: true,
       },
     },
